6e82bf105f330773b63ecee3de365da404eed4dc
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-07-10 20:42:01 -0700

    seeded postgres DB

--- db/postgreSQL/seeds/schema.sql
+++ db/postgreSQL/seeds/schema.sql
@@ -4,31 +4,36 @@ CREATE DATABASE reviewModule;
 
 \c reviewModule;
 
+DROP TABLE IF EXISTS products CASCADE;
+
 CREATE TABLE products(
   id INTEGER PRIMARY KEY,
   productName VARCHAR(255)
- );
+);
+
+-- cascase option allows removal foreign key, because it depends on
+DROP TABLE IF EXISTS reviews CASCADE;
 
 CREATE TABLE reviews(
-   id INTEGER PRIMARY KEY,
-   product_id INTEGER,
-   reviewTitle VARCHAR(255),
-   reviewText TEXT,
-   rating DECIMAL NULL,
-   bottomLine VARCHAR(200),
-   votes_down SMALLINT,
-   votes_up SMALLINT,
-   verified_buyer BOOLEAN,
-   reviewTime VARCHAR(100),
-   firstName VARCHAR(255),
-   lastName VARCHAR(255),
-   ageRange VARCHAR(100),
-   place VARCHAR(255),
-   skinType VARCHAR(255),
-   skinShade VARCHAR(255),
-   FOREIGN KEY (product_id)
-     REFERENCES products(id)
- );
+  id INTEGER PRIMARY KEY,
+  product_id INTEGER,
+  reviewTitle VARCHAR(255),
+  reviewText TEXT,
+  rating DECIMAL NULL,
+  bottomLine VARCHAR(200),
+  votes_down SMALLINT,
+  votes_up SMALLINT,
+  verified_buyer BOOLEAN,
+  reviewTime VARCHAR(100),
+  firstName VARCHAR(255),
+  lastName VARCHAR(255),
+  ageRange VARCHAR(100),
+  place VARCHAR(255),
+  skinType VARCHAR(255),
+  skinShade VARCHAR(255),
+  FOREIGN KEY (product_id)
+    REFERENCES products(id)
+);
 
 COPY products(id,productName)
-FROM './products.csv' DELIMITER ',' CSV HEADER;
+FROM '/Users/rizwanchoudhury/Desktop/Hack Reactor/Hack Reactor Actual Course Projects/SDC/Reviews-module/DB/postgreSQL/seeds/products.csv' DELIMITER ',' CSV HEADER;
