66da8510aa954842eee711eedb22c05f9d38811b
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-07-13 12:55:31 -0700

    convert model to fetch data from mongo, 4 more functions to go

--- db/mongoDB/reviews/reviewsModel.js
+++ db/mongoDB/reviews/reviewsModel.js
@@ -4,44 +4,31 @@ const Products = require('../index.js');
 // Id must be analyzed to see if it is a numeric id, or a product name. Based on this, one of 2 query strings will be chosen.
 const model = {
   getByProdId: (id, callback) => {
-    // Execute query by product ID if id is  number
-    if (parseInt(id) !== NaN) {
-      // console.log(`Product ID : ${id}`);
-      // Must specify each column, as I don't want user password and other fields being sent back
 
-      Products.find({ productId=id })
-      db.query(`SELECT reviewId, productId, productName, user_id, reviewTitle, reviewText, rating, bottomLine, votes_down, votes_up, verified_buyer, reviewTime, firstName, lastName, ageRange, place, skinType, skinShade FROM reviews INNER JOIN users ON reviews.user_id = users.id WHERE productId=${id};`, (err, result) => {
-        if (err) {
-          // eslint-disable-next-line no-console
-          console.error(err);
-          callback(err, null);
-        } else {
-          // console.log('By id result : ', result);
-          callback(null, result);
-        }
-      });
-      // Else execute query by product name,
+    // Execute query by product ID if id is number
+    // Else execute query by product name
+    if (parseInt(id) !== NaN) {
+
+      Products.find({ productId: id },
+        (err, result) => {
+          (err) ? callback(err, null) : callback(null, result);
+        });
+
     } else {
-      // console.log(`Product name : ${id}`);
-      db.query(`SELECT reviewId, productId, productName, user_id, reviewTitle, reviewText, rating, bottomLine, votes_down, votes_up, verified_buyer, reviewTime, firstName, lastName, ageRange, place, skinType, skinShade FROM reviews INNER JOIN users ON reviews.user_id = users.id WHERE productName="${id}";`, (err, result) => {
-        if (err) { callback(err, null); } else {
-          // console.log('By name result : ', result);
-          callback(null, result);
-        }
-      });
+
+      Products.find({ productName: id },
+        (err, result) => {
+          (err) ? callback(err, null) : callback(null, result);
+        });
     }
   },
   getBySkinType: (id, skinType, callback) => {
-    db.query(`SELECT reviewId, productId, productName, user_id, reviewTitle, reviewText, rating, bottomLine, votes_down, votes_up, verified_buyer, reviewTime, firstName, lastName, ageRange, place, skinType, skinShade FROM reviews INNER JOIN users ON reviews.user_id = users.id WHERE productId="${id}" AND skinType="${skinType}";`, (err, result) => {
-      if (err) {
-        // eslint-disable-next-line no-console
-        console.error(err);
-        callback(err, null);
-      } else {
-        // console.log('result by skinType :', result);
-        callback(null, result);
-      }
-    });
+
+    Products.find({ productId: id, skinType: skinType },
+      (err, result) => {
+        (err) ? callback(err, null) : callback(null, result);
+      });
+
   },
   searchReviews: (id, queryString, callback) => {
     // console.log(id, queryString);
@@ -70,48 +57,42 @@ const model = {
     });
   },
   getBySkinShade: (id, skinShade, callback) => {
-    db.query(`SELECT reviewId, productId, productName, user_id, reviewTitle, reviewText, rating, bottomLine, votes_down, votes_up, verified_buyer, reviewTime, firstName, lastName, ageRange, place, skinType, skinShade FROM reviews INNER JOIN users ON reviews.user_id = users.id WHERE productId="${id}" AND skinShade="${skinShade}";`, (err, result) => {
-      if (err) {
-        // eslint-disable-next-line no-console
-        console.error(err);
-        callback(err, null);
-      } else {
-        callback(null, result);
-      }
-    });
+
+    Products.find({ productId: id, skinShade: skinShade },
+      (err, result) => {
+        (err) ? callback(err, null) : callback(null, result);
+      });
+
   },
   getByAgeRange: (id, ageRange, callback) => {
-    db.query(`SELECT reviewId, productId, productName, user_id, reviewTitle, reviewText, rating, bottomLine, votes_down, votes_up, verified_buyer, reviewTime, firstName, lastName, ageRange, place, skinType, skinShade FROM reviews INNER JOIN users ON reviews.user_id = users.id WHERE productId="${id}" AND ageRange="${ageRange}";`, (err, result) => {
-      if (err) {
-        // eslint-disable-next-line no-console
-        console.error(err);
-        callback(err, null);
-      } else {
-        callback(null, result);
-      }
-    });
+
+    Products.find({ productId: id, ageRange: ageRange },
+      (err, result) => {
+        (err) ? callback(err, null) : callback(null, result);
+      });
+
   },
   postUpVote: (id, callback) => {
-    db.query(`UPDATE reviews SET votes_up = votes_up + 1 WHERE reviewId=${id}`, (err, result) => {
-      if (err) {
-        // eslint-disable-next-line no-console
-        console.error(err);
-        callback(err, null);
-      } else {
-        callback(null, result);
-      }
-    });
+
+    Products.findOneAndUpdate(
+      { reviewId: id },
+      { $inc: { votesUp: 1 } },
+      { new: true },
+      (err, result) => {
+        (err) ? callback(err, null) : callback(null, result);
+      });
+
   },
   postDownVote: (id, callback) => {
-    db.query(`UPDATE reviews SET votes_down = votes_down + 1 WHERE reviewId=${id}`, (err, result) => {
-      if (err) {
-        // eslint-disable-next-line no-console
-        console.error(err);
-        callback(err, null);
-      } else {
-        callback(null, result);
-      }
-    });
+
+    Products.findOneAndUpdate(
+      { reviewId: id },
+      { $inc: { votesDown: 1 } },
+      { new: true },
+      (err, result) => {
+        (err) ? callback(err, null) : callback(null, result);
+      });
+
   },
   postNewReview: (body, id, callback) => {
     let queryStr = `INSERT INTO reviews (productId, productName, user_id, reviewTitle, reviewText, rating, bottomLine, votes_down, votes_up, verified_buyer, reviewTime) VALUES (${id}, "Lash Stick", 3, "A waste of practical Lash Stick!", "${body.reviewText}", 2.2, "No - I would not recommend this to a friend", 13, 5, false, "5/1/2011");`;
