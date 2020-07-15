993e6d879858d406deed3f654fc8da5f3985891b
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-07-15 11:53:59 -0700

    Revert "Mongo api"

--- db/mongoDB/index.js
+++ db/mongoDB/index.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 let database = 'mongodb://localhost/reviewSDC';
 
-mongoose.connect(database, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}, (err) => {
+mongoose.connect(database, { useNewUrlParser: true }, (err) => {
   (err) ?
     console.log('Could not connect to mongodb') :
     console.log('Connected to reviewSDC');
@@ -14,26 +11,32 @@ mongoose.connect(database, {
 const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
-  reviewId: {
+  id: {
     type: Number,
-    unique: true
+    unique: true,
+    index: true
   },
-  productId: Number,
-  productName: { type: String },
-  reviewTitle: { type: String },
-  reviewText: { type: String },
-  rating: Number,
-  bottomLine: { type: String, index: true },
-  votesDown: Number,
-  votesUp: Number,
-  verifiedBuyer: String,
-  reviewTime: Date,
-  firstName: String,
-  lastName: String,
-  ageRange: String,
-  place: String,
-  skinType: String,
-  skinShade: String
+  productName: String,
+  reviews: [{
+    reviewId: {
+      type: Number,
+      index: true
+    },
+    reviewTitle: String,
+    reviewText: String,
+    rating: Number,
+    bottomLine: String,
+    votes_down: Number,
+    votes_up: Number,
+    verified_buyer: String,
+    reviewTime: Date,
+    firstName: String,
+    lastName: String,
+    ageRange: String,
+    place: String,
+    skinType: String,
+    skinShade: String
+  }]
 });
 
 const Products = mongoose.model("Products", ProductSchema);
--- server/routers/mongoController.js
+++ server/routers/mongoController.js
@@ -2,12 +2,12 @@ const model = require('../../DB/mongoDB/reviews/reviewsModel.js');
 
 const controller = {
   getByProdId: (req, res) => {
-
     model.getByProdId(req.params.productId, (err, result) => {
-      (err) ?
-        res.status(400).send(err) :
+      if (err) {
+        res.status(400).send(err);
+      } else {
         res.status(200).json(result);
-
+      }
     });
   },
   getByProdIdSort: (req, res) => {
@@ -36,68 +36,69 @@ const controller = {
       column = 'votes_up';
       order = 'DESC';
     }
-    // console.log(req.params)
-
     model.getByProdIdSort(req.params.productId, column, order, (err, result) => {
-      (err) ?
-        res.status(400).send(err) :
+      if (err) {
+        res.status(400).send(err);
+      } else {
         res.status(200).json(result);
+      }
     });
   },
   searchReviews: (req, res) => {
-    let { productId, queryStr } = req.params;
-
-    model.searchReviews(productId, queryStr, (err, result) => {
-      (err) ?
-        res.status(400).send(err) :
+    model.searchReviews(req.params.productId, req.params.queryStr, (err, result) => {
+      if (err) {
+        res.status(400).send(err);
+      } else {
         res.status(200).json(result);
+      }
     });
   },
   getBySkinType: (req, res) => {
-    let { productId, skinType } = req.params;
-
-    model.getBySkinType(productId, skinType, (err, result) => {
-      (err) ?
-        res.status(400).send(err) :
+    model.getBySkinType(req.params.productId, req.params.skinType, (err, result) => {
+      if (err) {
+        res.status(400).send(err);
+      } else {
         res.status(200).json(result);
+      }
     });
   },
   getBySkinShade: (req, res) => {
-    let { productId, skinShade } = req.params;
-
-    model.getBySkinShade(productId, skinShade, (err, result) => {
-      (err) ?
-        res.status(400).send(err) :
+    model.getBySkinShade(req.params.productId, req.params.skinShade, (err, result) => {
+      if (err) {
+        res.status(400).send(err);
+      } else {
         res.status(200).json(result);
+      }
     });
   },
   getByAgeRange: (req, res) => {
-    let { productId, ageRange } = req.params;
-
-    model.getByAgeRange(productId, ageRange, (err, result) => {
-      (err) ?
-        res.status(400).send(err) :
+    model.getByAgeRange(req.params.productId, req.params.ageRange, (err, result) => {
+      if (err) {
+        res.status(400).send(err);
+      } else {
         res.status(200).json(result);
+      }
     });
   },
   postUpVote: (req, res) => {
-
     model.postUpVote(req.params.reviewId, (err, result) => {
-      (err) ?
-        res.status(400).send(err) :
+      if (err) {
+        res.status(400).send(err);
+      } else {
         res.status(200).json(result);
+      }
     });
   },
   postDownVote: (req, res) => {
-
     model.postDownVote(req.params.reviewId, (err, result) => {
-      (err) ?
-        res.status(400).send(err) :
+      if (err) {
+        res.status(400).send(err);
+      } else {
         res.status(200).json(result);
+      }
     });
   },
   postNewReview: (req, res) => {
-
     // req.body must have a key called 'reviewText'.
     model.postNewReview(req.body, req.params.productId, (err, result) => {
       (err) ?
@@ -106,7 +107,6 @@ const controller = {
     });
   },
   deleteReviewById: (req, res) => {
-
     model.deleteReviewById(req.params.reviewId, (err, result) => {
       (err) ?
         res.status(400).send(err) :
