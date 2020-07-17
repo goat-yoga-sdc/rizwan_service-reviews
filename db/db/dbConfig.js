61311bfb31b50e169f0eee045e37591480b35aec
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-07-16 19:36:37 -0700

    Get rid of database except postgres

--- db/db/dbConfig.js
+++
@@ -1,1 +0,0 @@
-module.exports = 'helloKitty';
--- db/db/example.dbConfig.js
+++
@@ -1,2 +0,0 @@
-// Change 'your_db_password', and remove '.example' from beginning of filepath, so it matches the .gitignore
-module.exports = 'your_db_password';
